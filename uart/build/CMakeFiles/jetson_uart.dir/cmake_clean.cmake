01229ed9042ec56578fe772b6869bc65c51c1c18
Linh Nguyen <hoangtuboy95@gmail.com>
2018-01-31 13:29:24 +0700

    create uart

---
+++ uart/build/CMakeFiles/jetson_uart.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/jetson_uart.dir/src/main.cpp.o"
+  "jetson_uart.pdb"
+  "jetson_uart"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/jetson_uart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
